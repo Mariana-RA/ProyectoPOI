0c1c3f060a1cf1e095b99933ecebe7b5c2dd8187
Mariana-RA <mariana.regaladoa@uanl.edu.mx>
2025-10-18 15:02:35 -0600

    Se agrego la base de datos a CleverCloud

--- scripts/conexion.js
+++ scripts/conexion.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    database: "Quiniela",
-    user: "root",
-    password: "Mar1234!#",
+    host: "bt6mk6zfuzap4xdwukvi-mysql.services.clever-cloud.com",
+    database: "bt6mk6zfuzap4xdwukvi",
+    user: "ukkztbnntqbp2hqb",
+    password: "PDXhmghFHR3pv2sTLn3O",
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
